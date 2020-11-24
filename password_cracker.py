fe657a0d5a044cd767224e7da377dd4289f2a71f
Simon Cooper <kidney9@hotmail.com>
2020-11-24 17:23:46 +0000

    crack_sha1_hash function completed

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,23 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+with open('top-10000-passwords.txt') as passwords_file:
+  passwords = passwords_file.read().splitlines()
+
+with open('known-salts.txt') as salts_file:
+  salts = salts_file.read().splitlines()
+
+def hash_matches(password, hash):
+  return hashlib.sha1(password.encode()).hexdigest() == hash
+
+def crack_sha1_hash(hash, use_salts=False):
+  for password in passwords:
+    if use_salts:
+      for salt in salts:
+        if hash_matches(salt + password, hash) |            hash_matches(password + salt, hash):
+          return password
+
+    else:
+      if hash_matches(password, hash):
+        return password
+  
+  return 'PASSWORD NOT IN DATABASE'
