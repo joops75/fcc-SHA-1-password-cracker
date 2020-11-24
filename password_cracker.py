d54786f550c91a76cd73c0a208faf38e2e3e527f
Simon Cooper <kidney9@hotmail.com>
2020-11-24 19:04:32 +0000

    iterate over files in crack_sha1_hash function

--- password_cracker.py
+++ password_cracker.py
@@ -1,23 +1,21 @@
 import hashlib
 
-with open('top-10000-passwords.txt') as passwords_file:
-  passwords = passwords_file.read().splitlines()
-
-with open('known-salts.txt') as salts_file:
-  salts = salts_file.read().splitlines()
-
 def hash_matches(password, hash):
   return hashlib.sha1(password.encode()).hexdigest() == hash
 
 def crack_sha1_hash(hash, use_salts=False):
-  for password in passwords:
-    if use_salts:
-      for salt in salts:
-        if hash_matches(salt + password, hash) |            hash_matches(password + salt, hash):
-          return password
+  with open('top-10000-passwords.txt') as passwords_file:
+    for line in passwords_file:
+      password = line.rstrip('\n\r')
+      if use_salts:
+        with open('known-salts.txt') as salts_file:
+          for line in salts_file:
+            salt = line.rstrip('\n\r')
+            if hash_matches(salt + password, hash) |        hash_matches(password + salt, hash):
+              return password
 
-    else:
-      if hash_matches(password, hash):
-        return password
+      else:
+        if hash_matches(password, hash):
+          return password
   
   return 'PASSWORD NOT IN DATABASE'
